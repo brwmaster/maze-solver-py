63e0f39684e95e7d7e9e47a6de3ed2580c096cb6
Edwin Boon <edwin.boon@eo.nl>
2024-01-16 09:40:04 +0100

    Few minor tweaks

--- window.py
+++ window.py
@@ -2,13 +2,11 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width: int, height: int) -> None:
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.title = "MazeSolver Py"
-        self.canvas = Canvas(width=self.width, height=self.height)
-        self.canvas.pack()
+        self.canvas = Canvas(self.__root, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
         self.window_is_running = False
 
     def redraw(self) -> None:
